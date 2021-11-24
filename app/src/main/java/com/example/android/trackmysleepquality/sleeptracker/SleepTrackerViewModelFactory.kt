5dc914f4cb80e33bb1c23ed030b7387a19b7e750
Ainun <ayi.jayz13@gmail.com>
2021-11-24 10:49:13 +0800

    Added \Coroutines and Room

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModelFactory.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModelFactory.kt
@@ -29,11 +29,11 @@ import com.example.android.trackmysleepquality.database.SleepDatabaseDao
 class SleepTrackerViewModelFactory(
         private val dataSource: SleepDatabaseDao,
         private val application: Application) : ViewModelProvider.Factory {
-    @Suppress("unchecked_cast")
-    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-        if (modelClass.isAssignableFrom(SleepTrackerViewModel::class.java)) {
-            return SleepTrackerViewModel(dataSource, application) as T
-        }
+        @Suppress("unchecked_cast")
+        override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(SleepTrackerViewModel::class.java)) {
+                return SleepTrackerViewModel(dataSource, application) as T
+            }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
 }
